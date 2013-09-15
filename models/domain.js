22fb9fe10be7e0863f63a0ee74332699ebc9795c
Phoenix Nemo <mrphoenixlzx@gmail.com>
2013-09-15 12:21:49 +0800

    decrease SOA for faster update

--- models/domain.js
+++ models/domain.js
@@ -58,7 +58,7 @@ Domain.prototype.save = function(callback) {
                     "domain_id": domain.id,
                     "name": domain.name,
                     "type": "SOA",
-                    "content": config.powerservers[0] + ' ' + config.adminMail + ' 0 14400 3600 604800 ' + config.powerttl,
+                    "content": config.powerservers[0] + ' ' + config.adminMail + ' 0 3600 180 1209600 ' + config.powerttl,
                     "ttl": config.powerttl
                 });
                 myConnection.release();

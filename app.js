cffaa31d906313355199e792b160537d94e68807
Phoenix Nemo <mrphoenixlzx@gmail.com>
2014-06-18 21:19:28 +0800

    update less-middleware usage

--- app.js
+++ app.js
@@ -44,11 +44,7 @@ app.use(express.csrf());
 app.use(i18n.init);
 app.use(flash());
 app.use(app.router);
-app.use(require('less-middleware')({
-    dest: __dirname + '/public/',
-    src: __dirname + '/public',
-    compress: true
-}));
+app.use(lessMiddleware(path.join(__dirname, '/public')));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(function(req, res) {
